24d2793e6c067b24e7eaeec21b1922f0d2912769
Beats <Beats@DESKTOP-AHAJC1T>
2018-04-17 16:09:15 +0800

    项目设计文档-需求分析、类图、数据库结构图

---
+++ share/db/examonline.sql
@@ -0,0 +1,102 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.20 : Database - examonline
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`examonline` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `examonline`;
+
+/*Table structure for table `exam` */
+
+DROP TABLE IF EXISTS `exam`;
+
+CREATE TABLE `exam` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `subject` varchar(32) NOT NULL,
+  `startTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `endTime` datetime NOT NULL,
+  `state` varchar(16) DEFAULT 'create',
+  `t_id` varchar(16) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_EXAM_TEACHER` (`t_id`),
+  CONSTRAINT `FK_EXAM_TEACHER` FOREIGN KEY (`t_id`) REFERENCES `teacher` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `exam` */
+
+/*Table structure for table `question` */
+
+DROP TABLE IF EXISTS `question`;
+
+CREATE TABLE `question` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `number` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `answer` text NOT NULL,
+  `e_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_QUESTION_EXAM` (`e_id`),
+  CONSTRAINT `FK_QUESTION_EXAM` FOREIGN KEY (`e_id`) REFERENCES `exam` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `question` */
+
+/*Table structure for table `selection` */
+
+DROP TABLE IF EXISTS `selection`;
+
+CREATE TABLE `selection` (
+  `id` int(11) NOT NULL,
+  `q_id` int(11) NOT NULL,
+  `item` text NOT NULL,
+  PRIMARY KEY (`id`),
+  CONSTRAINT `FK_SELECTIO_QUESTION` FOREIGN KEY (`id`) REFERENCES `question` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `selection` */
+
+/*Table structure for table `student` */
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` varchar(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `ip` varchar(32) DEFAULT '0.0.0.0',
+  `clazz` varchar(32) DEFAULT NULL,
+  `e_id` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `student` */
+
+/*Table structure for table `teacher` */
+
+DROP TABLE IF EXISTS `teacher`;
+
+CREATE TABLE `teacher` (
+  `id` varchar(16) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `passwd` varchar(32) DEFAULT NULL,
+  `isAdmin` tinyint(1) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `teacher` */
+
+insert  into `teacher`(`id`,`name`,`passwd`,`isAdmin`) values ('root','root','63a9f0ea7bb98050796b649e85481845',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
