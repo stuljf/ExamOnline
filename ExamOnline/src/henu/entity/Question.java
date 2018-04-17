a15bf6de71af0a7a82ffae6a183344835624590e
Beats <Beats@DESKTOP-AHAJC1T>
2018-04-18 00:15:46 +0800

    更新： 1）添加Spring对junit的支持以及使用案例 2）加入Json转换工具类 3）修改试卷试题选项的类型：由原来的List<String>变成String，保存格式：[{"A":"A选项"},{"B":"B选项"},{"C":"C选项"},{"D":"D选项"}]

--- ExamOnline/src/henu/entity/Question.java
+++ ExamOnline/src/henu/entity/Question.java
@@ -1,7 +1,5 @@
 package henu.entity;
 
-import java.util.List;
-
 import org.codehaus.jackson.annotate.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown=true)
@@ -10,54 +8,50 @@ public class Question {
     private Integer id;
     private Integer number;
     private String title;
-    private List<String> selections;
+    private String type;
+    private String selection;
     private String answer;
     private Integer e_id;
-
+    
+    public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getSelection() {
+		return selection;
+	}
+	public void setSelection(String selection) {
+		this.selection = selection;
+	}
     public String getTitle() {
 		return title;
 	}
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-	public List<String> getSelections() {
-		return selections;
-	}
-
-	public void setSelections(List<String> selections) {
-		this.selections = selections;
-	}
-
 	public String getAnswer() {
 		return answer;
 	}
-
 	public void setAnswer(String answer) {
 		this.answer = answer;
 	}
-
 	public Integer getE_id() {
 		return e_id;
 	}
-
 	public void setE_id(Integer e_id) {
 		this.e_id = e_id;
 	}
-
 	public Integer getId() {
         return id;
     }
-
     public void setId(Integer id) {
         this.id = id;
     }
-
     public Integer getNumber() {
         return number;
     }
-
     public void setNumber(Integer number) {
         this.number = number;
     }
