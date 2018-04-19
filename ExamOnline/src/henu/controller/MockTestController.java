64a850476acbf98515718111b8af03a935eb9bf2
Beats <794322460@qq.com>
2018-04-19 14:24:59 +0800

    4.19更新： 1）数据库修改，试卷题目的选项采用json文本格式存储 2）添加Mock测试框架的使用案例

---
+++ ExamOnline/src/henu/controller/MockTestController.java
@@ -0,0 +1,34 @@
+package henu.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import henu.entity.Exam;
+
+/**
+ * @ClassName: MockTestController <br/> 
+ * @Describtion: Mock框架测试用例. <br/> 
+ * @date: 2018年4月19日 下午2:02:47 <br/> 
+ * @author Beats <br/> 
+ * @version v1.0
+ */
+@Controller
+public class MockTestController {
+
+	@RequestMapping("/hello")
+	@ResponseBody
+	public String hello(String hello) {
+		return hello + " world!";
+	}
+	
+	@RequestMapping("/json")
+	@ResponseBody
+	public Exam json(String json) {
+		Exam e = new Exam();
+		e.setId(123);
+		e.setSubject(json);
+		return e;
+	}
+	
+}
